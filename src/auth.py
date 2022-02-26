6fc3f0c7cb8915b52859e328b59c06d912ef636e
Hanqi Bai <z5286125@ad.unsw.edu.au>
2022-02-26 20:06:15 +1100

    remove local test

--- src/auth.py
+++ src/auth.py
@@ -1,10 +1,10 @@
-# from src.data_store import data_store
-# from src.error import InputError
+from src.data_store import data_store
+from src.error import InputError
 import re
-import pytest
+# import pytest
 
-from data_store import data_store   # del
-from error import InputError        # del
+# from data_store import data_store   # del
+# from error import InputError        # del
 
 
 # Arguments:
@@ -144,51 +144,51 @@ def auth_register_v1(email, password, name_first, name_last):
     
 
 
-# del
-if __name__ == '__main__':
-    print("==============================================")
-    # print(check_email_valid('z123456@unsw.edu.au'))
-    # print(creat_handle('Hanqi@', '%BAI'))
-    # print(creat_handle('qqqqqqqqqq@#$', 'qqqqqqqqqq@#$'))
-    # print(auth_login_v1('123a@.com', '123123123'))
-    # print(auth_login_v1('123@.com', '1231231'))
-    # print(auth_login_v1('123@.com', '123123123'))
-    store = data_store.get()
-    store['users'].append(('z7654321@ed.unsw.edu.au', '1234567', 'Jason', 'Smith', 1, 'jasonsmith'))
-    store['users'].append(('z5555555@ed.unsw.edu.au', '123123123', 'William', 'Wu', 2, 'williamwu'))
-    store['users'].append(('z8888888@ed.unsw.edu.au', '321321321', 'Russell', 'Wang', 3, 'russellwu'))
-    # print(auth_login_v1('123a@.com', '123123123'))
-    # print(auth_login_v1('z7654321@ed.unsw.edu.au', '123123'))
-    # print(auth_login_v1('z7654321@ed.unsw.edu.au', '1234567'))
-    # print(store['users'])
-    with pytest.raises(InputError):
-        assert auth_login_v1('z1234567@ed.unsw.edu.au', '1234567')
-    with pytest.raises(InputError):
-        assert auth_login_v1('z7654321@ed.unsw.edu.au', '1111111')
-    assert auth_login_v1('z7654321@ed.unsw.edu.au', '1234567') == {1}
-    assert auth_login_v1('z8888888@ed.unsw.edu.au', '321321321') == {3}
-    with pytest.raises(InputError):
-        assert auth_register_v1('z1234567', '1234567', 'Donald', 'Trump')
-    with pytest.raises(InputError):
-        assert auth_register_v1('z1234567ed.unsw.edu.au', '1234567', 'Donald', 'Trump')
-    with pytest.raises(InputError):
-        assert auth_register_v1('z1234567@ed', '1234567', 'Donald', 'Trump')
-    with pytest.raises(InputError):
-        assert auth_register_v1('z7654321ed.unsw.edu.au', '1234567', 'Donald', 'Trump')
-    with pytest.raises(InputError):
-        assert auth_register_v1('z1234567ed.unsw.edu.au', '1', 'Donald', 'Trump')
-    with pytest.raises(InputError):
-        assert auth_register_v1('z1234567ed.unsw.edu.au', '1234567', '', 'Trump')
-    first_name_long = 'qweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqwe'
-    with pytest.raises(InputError):
-        assert auth_register_v1('z1234567ed.unsw.edu.au', '1234567', first_name_long, 'Trump')
-    with pytest.raises(InputError):
-        assert auth_register_v1('z1234567ed.unsw.edu.au', '1234567', 'Donald', '')
-    last_name_long = 'qweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqwe'
-    with pytest.raises(InputError):
-        assert auth_register_v1('z1234567ed.unsw.edu.au', '1234567', 'Donald', last_name_long)
-    assert auth_register_v1('z1234567@ed.unsw.edu.au', '1234567', 'Donald', 'Trump') == {4}
-    assert auth_register_v1('z11231237@ed.unsw.edu.au', '1234567', 'qqqqqqqqqq', 'qqqqqqqqqq') == {5}
-    assert auth_register_v1('z1546267@ed.unsw.edu.au', '1234567', 'qqqqqqqqqq', 'qqqqqqqqqq') == {6}
-    print(store['users'])
-    print("==============================================")
+# # del
+# if __name__ == '__main__':
+#     print("==============================================")
+#     # print(check_email_valid('z123456@unsw.edu.au'))
+#     # print(creat_handle('Hanqi@', '%BAI'))
+#     # print(creat_handle('qqqqqqqqqq@#$', 'qqqqqqqqqq@#$'))
+#     # print(auth_login_v1('123a@.com', '123123123'))
+#     # print(auth_login_v1('123@.com', '1231231'))
+#     # print(auth_login_v1('123@.com', '123123123'))
+#     store = data_store.get()
+#     store['users'].append(('z7654321@ed.unsw.edu.au', '1234567', 'Jason', 'Smith', 1, 'jasonsmith'))
+#     store['users'].append(('z5555555@ed.unsw.edu.au', '123123123', 'William', 'Wu', 2, 'williamwu'))
+#     store['users'].append(('z8888888@ed.unsw.edu.au', '321321321', 'Russell', 'Wang', 3, 'russellwu'))
+#     # print(auth_login_v1('123a@.com', '123123123'))
+#     # print(auth_login_v1('z7654321@ed.unsw.edu.au', '123123'))
+#     # print(auth_login_v1('z7654321@ed.unsw.edu.au', '1234567'))
+#     # print(store['users'])
+#     with pytest.raises(InputError):
+#         assert auth_login_v1('z1234567@ed.unsw.edu.au', '1234567')
+#     with pytest.raises(InputError):
+#         assert auth_login_v1('z7654321@ed.unsw.edu.au', '1111111')
+#     assert auth_login_v1('z7654321@ed.unsw.edu.au', '1234567') == {1}
+#     assert auth_login_v1('z8888888@ed.unsw.edu.au', '321321321') == {3}
+#     with pytest.raises(InputError):
+#         assert auth_register_v1('z1234567', '1234567', 'Donald', 'Trump')
+#     with pytest.raises(InputError):
+#         assert auth_register_v1('z1234567ed.unsw.edu.au', '1234567', 'Donald', 'Trump')
+#     with pytest.raises(InputError):
+#         assert auth_register_v1('z1234567@ed', '1234567', 'Donald', 'Trump')
+#     with pytest.raises(InputError):
+#         assert auth_register_v1('z7654321ed.unsw.edu.au', '1234567', 'Donald', 'Trump')
+#     with pytest.raises(InputError):
+#         assert auth_register_v1('z1234567ed.unsw.edu.au', '1', 'Donald', 'Trump')
+#     with pytest.raises(InputError):
+#         assert auth_register_v1('z1234567ed.unsw.edu.au', '1234567', '', 'Trump')
+#     first_name_long = 'qweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqwe'
+#     with pytest.raises(InputError):
+#         assert auth_register_v1('z1234567ed.unsw.edu.au', '1234567', first_name_long, 'Trump')
+#     with pytest.raises(InputError):
+#         assert auth_register_v1('z1234567ed.unsw.edu.au', '1234567', 'Donald', '')
+#     last_name_long = 'qweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqwe'
+#     with pytest.raises(InputError):
+#         assert auth_register_v1('z1234567ed.unsw.edu.au', '1234567', 'Donald', last_name_long)
+#     assert auth_register_v1('z1234567@ed.unsw.edu.au', '1234567', 'Donald', 'Trump') == {4}
+#     assert auth_register_v1('z11231237@ed.unsw.edu.au', '1234567', 'qqqqqqqqqq', 'qqqqqqqqqq') == {5}
+#     assert auth_register_v1('z1546267@ed.unsw.edu.au', '1234567', 'qqqqqqqqqq', 'qqqqqqqqqq') == {6}
+#     print(store['users'])
+#     print("==============================================")
