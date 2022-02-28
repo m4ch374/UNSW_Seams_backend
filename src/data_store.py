ee49c5dccf441147b271eb87cbcb2e600dceb63a
Ella Huang <z5358341@unsw.edu.au>
2022-02-28 13:02:17 +1100

    change channel to channels

--- src/data_store.py
+++ src/data_store.py
@@ -27,7 +27,7 @@ Example usage:
 ## YOU SHOULD MODIFY THIS OBJECT BELOW
 initial_object = {
     'users' : [], 
-    'channel' : [],
+    'channels' : [],
 } # credit to Hanqi for this placeholder love you <3
 
 ## YOU SHOULD MODIFY THIS OBJECT ABOVE
