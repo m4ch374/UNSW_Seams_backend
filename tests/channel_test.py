00a4a3606e7a07d9b7b4062f9c5ec56b7d3fb442
James <z5360589@ad.unsw.edu.au>
2022-03-02 21:10:15 +1100

    tests for channel_details_v1

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -34,7 +34,7 @@ def test_invalid_user_id_for_channel_details():
 
 # raise InputError since invalid channel_id passed
 #
-# note: one valid auth_id_is_created
+# note: one valid auth_id_is_created (but no channels)
 def test_invalid_channel_id():
     clear_v1()
     auth_user_id = auth_register_v1('z1234567@ad.unsw.edu.au', 'password', 'firstname', 'lastname');
@@ -43,7 +43,7 @@ def test_invalid_channel_id():
         channel_details_v1(auth_user_id, invalid_channel_id)
 
 # raise InputError since channel_id passed doesn't match channel created
-def test_channel_id_doesnt_match_valid_channel():
+def test_channel_id_doesnt_match_valid_channel_1():
     clear_v1()
     auth_user_id = auth_register_v1('z1234567@ad.unsw.edu.au', 'password', 'firstname', 'lastname');
     valid_channel_id = channels_create_v1(auth_user_id, 'First Channel', True)
@@ -53,36 +53,56 @@ def test_channel_id_doesnt_match_valid_channel():
     with pytest.raises(InputError):
         channel_details_v1(auth_user_id, invalid_channel_id)
 
-'''
-NOTE ADD MORE test_channel_id_doesnt_match_valid_channel functions here
-# delete once resolved
-'''
+# raise InputError since channel_id passed doesn't match any of channels created
+def test_channel_id_doesnt_match_valid_channel_2():
+    clear_v1()
+    auth_user_id1 = auth_register_v1('z1234567@ad.unsw.edu.au', 'password', 'firstname', 'lastname');
+    valid_channel_id1 = channels_create_v1(auth_user_id1, 'First Channel', True)
+    auth_user_id2 = auth_register_v1('z7654321@ad.unsw.edu.au', 'cat', 'dog', 'chicken');
+    valid_channel_id2 = channels_create_v1(auth_user_id2, 'Second Channel', True)
+    auth_user_id3 = auth_register_v1('z3141592@ad.unsw.edu.au', 'potato', 'firstname', 'lastname');
+    valid_channel_id3 = channels_create_v1(auth_user_id3, 'Third Channel', False)
+    
+    # create a channel id that doesn't match any of the created channels
+    invalid_channel_id = valid_channel_id1 + valid_channel_id2 + valid_channel_id3
+    with pytest.raises(InputError):
+        channel_details_v1(auth_user_id, invalid_channel_id)
 
-# create a single channel and list details with no errors
+# create a single private channel and list details with no errors
 def test_valid_channel_details_1():
     clear_v1()
     auth_user_id = auth_register_v1('z1234567@ad.unsw.edu.au', 'password', 'firstname', 'lastname');
     valid_channel_id = channels_create_v1(auth_user_id, 'First Channel', True)
     assert channel_details_v1(auth_user_id, valid_channel_id) == {'name': 'First Channel', 'is_public': True}
-    
-    '''
-    NOTE : i dont know why but the function stub for channel_details_v1 doesnt 
-    include the key 'is_public' in it's return, but the specifications say that
-    it should
-    # delete once resolved
-    '''
-# create several channels and list details with no errors
+
+# create a single public channel and list details with no errors
 def test_valid_channel_details_2():
+    clear_v1()
+    auth_user_id = auth_register_v1('z1234567@ad.unsw.edu.au', 'password', 'firstname', 'lastname');
+    valid_channel_id = channels_create_v1(auth_user_id, 'First Channel', False)
+    assert channel_details_v1(auth_user_id, valid_channel_id) == {'name': 'First Channel', 'is_public': False}
+
+# create several channels and list details with no errors
+def test_valid_channel_details_3():
     clear_v1()
     auth_user_id1 = auth_register_v1('z1234567@ad.unsw.edu.au', 'password', 'firstname', 'lastname');
     valid_channel_id1 = channels_create_v1(auth_user_id1, 'First Channel', True)
     
-    auth_user_id2 = auth_register_v1('z1234568@ad.unsw.edu.au', 'password', 'firstname', 'lastname');
+    auth_user_id2 = auth_register_v1('z7654321@ad.unsw.edu.au', 'password', 'firstname', 'lastname');
     valid_channel_id2 = channels_create_v1(auth_user_id2, 'Second Channel', True)
     
     assert channel_details_v1(auth_user_id1, valid_channel_id1) == {'name': 'First Channel', 'is_public': True}
     assert channel_details_v1(auth_user_id2, valid_channel_id2) == {'name': 'Second Channel', 'is_public': True}
 
+# add a person to channel via join function, and correctly list channel details
+    clear_v1()
+    auth_user_id1 = auth_register_v1('z1234567@ad.unsw.edu.au', 'password', 'firstname', 'lastname');
+    valid_channel_id1 = channels_create_v1(auth_user_id1, 'First Channel', True)
+    
+    auth_user_id2 = auth_register_v1('z1111111@ad.unsw.edu.au', 'ypspspsp', 'firstname', 'lastname');
+    channel_join_v1(auth_user_id2, valid_channel_id)
+    
+    assert channel_details_v1(auth_user_id2, valid_channel_id1) == {'name': 'First Channel', 'is_public': True}
 ####################################################
 ##          Tests for channel_join_v1             ##
 ####################################################
