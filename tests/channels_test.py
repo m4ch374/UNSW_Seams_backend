c2e85186403e0550aea7ae9dcb06c4f71f5dcb32
Henry Wan <z5338828@ad.unsw.edu.au>
2022-03-03 11:34:09 +0800

    error_list and names_list changed from fixture to definitions

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -13,7 +13,33 @@ import src.auth as auth
 
 from src.error import InputError
 from src.other import clear_v1
-from src.data_store import data_store
+
+# ================= Definitions ====================
+
+# A list of channel names that could cause errors
+ERROR_LIST = [
+        None,                               # NULL
+        "",                                 # Empty name
+        "abcdefghijklmnopqrstu",            # 21 chars
+        "...............................",  # non english chars
+        "                               ",  # whitespaces with len > 20
+        "123456789012345678901",            # numbers
+        "!!!!!!!ldjfljasdlkjklsdj894",      # special chars, english chars and nums
+    ]
+
+# A list of channel names that are valid
+NAMES_LIST = [
+        "a",                    # single character
+        "abcdef",               # english character
+        "123456",               # nums
+        "there is something",   # str with whitespaces
+        "           ",          # str with all whitespaces
+        "1234567890abcdefg![]", # combined strings, len == 20
+        "trailing whitespace ", # str with trailing whitespace
+        "a",                    # duplications
+        "!!!!!!![[[]]]!!!!!!!", # all special chars
+    ]
+# ==================================================
 
 # =============== Global fixtures ==================
 # These are the fixtures that would be used accross
@@ -56,36 +82,6 @@ def channel_name():
 # ============= Channels create v1 =================
 # This section tests for the function
 # channels_create_v1()
-#
-# Returns a list of channels names that could cause error
-@pytest.fixture
-def error_list():
-    err_list = [
-        None,                               # NULL
-        "",                                 # Empty name
-        "abcdefghijklmnopqrstu",            # 21 chars
-        "...............................",  # non english chars
-        "                               ",  # whitespaces with len > 20
-        "123456789012345678901",            # numbers
-        "!!!!!!!ldjfljasdlkjklsdj894",      # special chars, english chars and nums
-    ]
-    return err_list
-
-# Returns a list of channel names that would not cause error
-@pytest.fixture
-def names_list():
-    names_list = [
-        "a",                    # single character
-        "abcdef",               # english character
-        "123456",               # nums
-        "there is something",   # str with whitespaces
-        "           ",          # str with all whitespaces
-        "1234567890abcdefg![]", # combined strings, len == 20
-        "trailing whitespace ", # str with trailing whitespace
-        "a",                    # duplications
-        "!!!!!!![[[]]]!!!!!!!", # all special chars
-    ]
-    return names_list
 
 # Should raise input error
 #
@@ -93,9 +89,9 @@ def names_list():
 #           Length of name > 20
 #
 # Test for creating public channels
-def test_channels_create_error_public(auth_user_id, error_list):
+def test_channels_create_error_public(auth_user_id):
     with pytest.raises(InputError):
-        for s in error_list:
+        for s in ERROR_LIST:
             chnl.channels_create_v1(auth_user_id, s, True)
 
 # Should raise input error
@@ -104,9 +100,9 @@ def test_channels_create_error_public(auth_user_id, error_list):
 #           Length of name > 20
 #
 # Test for creating private channels
-def test_channels_create_error_private(auth_user_id, error_list):
+def test_channels_create_error_private(auth_user_id):
     with pytest.raises(InputError):
-        for s in error_list:
+        for s in ERROR_LIST:
             chnl.channels_create_v1(auth_user_id, s, False)
 
 # Should raise input error
@@ -115,17 +111,17 @@ def test_channels_create_error_private(auth_user_id, error_list):
 #           Length of name > 20
 #
 # Test for creating both public and private channels
-def test_channels_create_error_pub_and_priv(auth_user_id, error_list):
+def test_channels_create_error_pub_and_priv(auth_user_id):
     with pytest.raises(InputError):
-        for s in error_list:
+        for s in ERROR_LIST:
             chnl.channels_create_v1(auth_user_id, s, True)
             chnl.channels_create_v1(auth_user_id, s, False)
 
 # Should not raise any error
 # 
 # Test for creating valid public channels
-def test_channels_create_public(auth_user_id, names_list):
-    for i, name in enumerate(names_list):
+def test_channels_create_public(auth_user_id):
+    for i, name in enumerate(NAMES_LIST):
         val = chnl.channels_create_v1(auth_user_id, name, True)
 
         assert val == { 'channel_id': i + 1 }
@@ -133,8 +129,8 @@ def test_channels_create_public(auth_user_id, names_list):
 # Should not raise any error
 # 
 # Test for creating valid private channels
-def test_channels_create_private(auth_user_id, names_list):
-    for i, name in enumerate(names_list):
+def test_channels_create_private(auth_user_id):
+    for i, name in enumerate(NAMES_LIST):
         val = chnl.channels_create_v1(auth_user_id, name, False)
 
         assert val == { 'channel_id': i + 1 }
@@ -142,8 +138,8 @@ def test_channels_create_private(auth_user_id, names_list):
 # Should not raise any error
 # 
 # Test for creating both valid public and private channels
-def test_channels_create_pub_and_priv(auth_user_id, names_list):
-    for i, name in enumerate(names_list):
+def test_channels_create_pub_and_priv(auth_user_id):
+    for i, name in enumerate(NAMES_LIST):
         val = chnl.channels_create_v1(auth_user_id, name, True)
         val_1 = chnl.channels_create_v1(auth_user_id, name, False)
 
