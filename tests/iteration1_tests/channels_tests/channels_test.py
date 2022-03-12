d19c9c9835ef7b0f046922aab0f4d31de4447670
Henry Wan <z5338828@ad.unsw.edu.au>
2022-03-12 21:56:13 +0800

    Fixtures: fixtures in the channels scoped are now shared

--- tests/iteration1_tests/channels_tests/channels_test.py
+++ tests/iteration1_tests/channels_tests/channels_test.py
@@ -36,39 +36,6 @@ NAMES_LIST = [
 INVALID_ID = -1
 # ==================================================
 
-# =============== Global fixtures ==================
-# These are the fixtures that would be used accross
-# all tests
-#
-# Runs clean_v1() before all tests
-@pytest.fixture(autouse=True)
-def clean():
-    clear_v1()
-
-# A dummy user id
-@pytest.fixture
-def auth_user_id():
-    #clear_v1()
-    auth_id = auth.auth_register_v1(
-        'z100@ed.unsw.edu.au', 
-        '1234567', 
-        'Donald', 
-        'Trump'
-    )
-    return auth_id['auth_user_id']
-
-# Returns another dummy user id
-@pytest.fixture
-def another_id():
-    auth_id = auth.auth_register_v1(
-        'z200@ed.unsw.edu.au', 
-        '1234567', 
-        'qqqqqqqqqq', 
-        'qqqqqqqqqq'
-    )
-    return auth_id['auth_user_id']
-# ==================================================
-
 # ============= Channels create v1 =================
 # This section tests for the function
 # channels_create_v1()
