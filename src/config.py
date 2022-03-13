f3c5ef6156679385224f8ff682445a330030b87d
Henry Wan <z5338828@ad.unsw.edu.au>
2022-03-14 01:58:59 +0800

    Definitions: Added token's secret

--- src/config.py
+++ src/config.py
@@ -1,3 +1,6 @@
 port = 8080
 
 url = f"http://localhost:{port}/"
+
+# A random 64 byte secret 
+TOKEN_SECRET = 'cdb53c23db72f44496625661d2bc417e5f47b0050b7869cd3c00976854ff12e8a6a77cfe3590326fd42a2ce0f2ee14e99f3654fd54c91333b4db51ebbfc8d3a3'
