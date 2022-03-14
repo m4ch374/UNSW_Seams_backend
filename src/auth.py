9643b700df1eefb64c91c8243d8920baa62762f8
Hanqi Bai <z5286125@ad.unsw.edu.au>
2022-03-14 22:33:23 +1100

    add uncompiled function

--- src/auth.py
+++ src/auth.py
@@ -3,7 +3,6 @@ from src.data_store import data_store
 from src.error import InputError
 from src.objecs import User
 
-
 '''
 Arguments:
     email (string)    - user's email
@@ -46,6 +45,30 @@ def auth_login_v1(email, password):
         user_id = state
         return {'auth_user_id': user_id}
 
+
+'''
+Arguments:
+    email (string)    - user's email
+    password (string)    - password for login
+
+Exceptions:
+    InputError  - Occurs when email entered does not belong to a user.
+                         when password is not correct.
+
+Return Value:
+    user's ID (integer) if the correct input
+    token (string)  Encrypted user id and time
+'''
+def auth_login_v2(email, password):
+    pass
+
+
+'''
+Arguments:
+    token (string)  Encrypted user id and time
+'''
+def auth_logout_v1(token):
+    pass
 
 '''
 Arguments:
@@ -108,3 +131,101 @@ def auth_register_v1(email, password, name_first, name_last):
         store['users'].append(new_user)
         data_store.set(store)
         return {'auth_user_id': new_user.id}         # return user's id
+
+
+'''
+Arguments:
+    email (string)    - user's email
+    password (string)    - user's password
+    name_first (string)    - user's first name
+    name_last (string)    - user's last name
+
+Exceptions:
+    InputError  - Occurs when email entered is not a valid email
+                         when email address is already exist
+                         when length of password is less than 6 characters
+                         when length of name_last/name_first is not between 1 and 50 characters inclusive
+
+Return Value:
+    user's ID (integer) on the correct input
+    token (string)  Encrypted user id and time
+'''
+def auth_register_v2(email, password, name_first, name_last):
+    pass
+
+
+'''
+Arguments:
+    token (string)  Encrypted user id and time
+
+Return Value:
+    users (list)    A list of all user's detail
+'''
+def users_all_v1(token):
+    pass
+
+
+'''
+Arguments:
+    token (string)  Encrypted user id and time
+    u_id (integer)    - user's id
+
+Exceptions:
+    InputError  - u_id does not refer to a valid user
+
+Return Value:
+    user's detail (dirt)    user's email, name, handle, id....
+'''
+def user_profile_v1(token, u_id):
+    pass
+
+
+'''
+Arguments:
+    token (string)  Encrypted user id and time
+    name_first (string)    - user's first name
+    name_last (string)    - user's last name
+
+Exceptions:
+    InputError  - Occurs
+    when length of name_last/name_first is not between 1 and 50 characters inclusive
+
+Return Value:
+    An empty dirt {}
+'''
+def user_profile_setname_v1(token, name_first, name_last):
+    pass
+
+
+'''
+Arguments:
+    token (string)  Encrypted user id and time
+    email (string)    - user's email
+
+Exceptions:
+    InputError  - Occurs    Email address not valid
+                            Email address already exists
+
+Return Value:
+    An empty dirt {}
+'''
+def user_profile_setemail_v1(token, email):
+    pass
+
+
+'''
+Arguments:
+    token (string)  Encrypted user id and time
+    handle_str (string)  new handle
+
+Exceptions:
+    InputError  - Occurs    Length of handle should between 3 and 20 characters
+                            Handle contains characters that are not alphanumeric
+                            Handle is already used by another user
+
+Return Value:
+    An empty dirt {}
+'''
+def user_profile_sethandle_v1(token, handle_str):
+    pass
+
