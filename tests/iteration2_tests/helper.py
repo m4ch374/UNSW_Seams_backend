b10e0ea0b720f3f48e4b00023859db8aaa1c32ac
James <z5360589@ad.unsw.edu.au>
2022-03-16 18:48:54 +1100

    added two helperfunctions

--- tests/iteration2_tests/helper.py
+++ tests/iteration2_tests/helper.py
@@ -8,3 +8,16 @@ def generate_channel_input_json(tok, name, is_public):
         'name': name,
         'is_public': is_public,
     }
+
+def generate_channel_join_input_json(token, channel_id):
+    return {
+        'token': token,
+        'channel_id': channel_id,
+    }
+
+def generate_channel_invite_input_json(token, channel_id, u_id):
+    return {
+        'token': token,
+        'channel_id': channel_id,
+        'u_id': u_id,
+    }
