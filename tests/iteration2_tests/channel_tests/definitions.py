fe346cbb1a4ea9ee1e7141935afaa2c30638850e
James <z5360589@ad.unsw.edu.au>
2022-03-17 10:22:48 +1100

    some strings used for testing invalid users/channels

---
+++ tests/iteration2_tests/channel_tests/definitions.py
@@ -0,0 +1,10 @@
+'''
+Definitions used for channel functions
+'''
+
+# Invalid token (used for channel functions testing)
+INVALID_TOKEN = "bleh"
+# Invalid Channel id (used when no valid channels have already been created)
+INVALID_CHNL_ID = -10000
+# Invalid user id (used when no valid users have already been created)
+INVALID_U_ID = -10000
