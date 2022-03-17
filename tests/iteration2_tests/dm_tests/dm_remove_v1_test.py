5a76099d57c5cb61ff771d1534d74116ce85eb98
Henry Wan <z5338828@ad.unsw.edu.au>
2022-03-17 16:57:00 +0800

    Pytest: added global owner has no permission test

--- tests/iteration2_tests/dm_tests/dm_remove_v1_test.py
+++ tests/iteration2_tests/dm_tests/dm_remove_v1_test.py
@@ -11,29 +11,22 @@ from src.error import InputError, AccessError
 from tests.iteration2_tests.endpoints import *
 
 # Import helper
-from tests.iteration2_tests.helper import generate_dm_input_json
+from tests.iteration2_tests.helper import generate_dm_input_json, generate_dm_json
 
 INVALID_TOKEN = 'invalid'
 INVALID_DM_ID = -1
-
-# Local helper
-def generate_dm_remove_json(tok, dm_id):
-    return {
-        'token': tok,
-        'dm_id': dm_id
-    }
 
 def test_invalid_token(get_token_1):
     data = generate_dm_input_json(get_token_1, [])
     dm_id = requests.post(ENDPOINT_DM_CREATE, json=data).json()['dm_id']
 
-    data = generate_dm_remove_json(INVALID_TOKEN, dm_id)
+    data = generate_dm_json(INVALID_TOKEN, dm_id)
     resp = requests.delete(ENDPOINT_DM_REMOVE, json=data)
 
     assert resp.status_code == AccessError.code
 
 def test_invalid_dm_id(get_token_1):
-    data = generate_dm_remove_json(get_token_1, INVALID_DM_ID)
+    data = generate_dm_json(get_token_1, INVALID_DM_ID)
     resp = requests.delete(ENDPOINT_DM_REMOVE, json=data)
     assert resp.status_code == InputError.code
 
@@ -41,7 +34,7 @@ def test_not_member(get_token_1, get_token_2):
     data = generate_dm_input_json(get_token_1, [])
     dm_id = requests.post(ENDPOINT_DM_CREATE, json=data).json()['dm_id']
 
-    data = generate_dm_remove_json(get_token_2, dm_id)
+    data = generate_dm_json(get_token_2, dm_id)
     resp = requests.delete(ENDPOINT_DM_REMOVE, json=data)
     assert resp.status_code == AccessError.code
 
@@ -49,7 +42,15 @@ def test_not_owner(get_token_1, get_usr_2):
     data = generate_dm_input_json(get_token_1, [get_usr_2['auth_user_id']])
     dm_id = requests.post(ENDPOINT_DM_CREATE, json=data).json()['dm_id']
 
-    data = generate_dm_remove_json(get_usr_2['token'], dm_id)
+    data = generate_dm_json(get_usr_2['token'], dm_id)
+    resp = requests.delete(ENDPOINT_DM_REMOVE, json=data)
+    assert resp.status_code == AccessError.code
+
+def test_global_owner_has_no_permission(get_usr_1, get_usr_2):
+    data = generate_dm_input_json(get_usr_2['token'], [])
+    dm_id = requests.post(ENDPOINT_DM_CREATE, json=data).json()['dm_id']
+
+    data = generate_dm_json(get_usr_1['token'], dm_id)
     resp = requests.delete(ENDPOINT_DM_REMOVE, json=data)
     assert resp.status_code == AccessError.code
 
@@ -75,7 +76,7 @@ def test_valid(get_usr_1, get_usr_2):
         },
     ]
 
-    data = generate_dm_remove_json(get_usr_1['token'], dm_id)
+    data = generate_dm_json(get_usr_1['token'], dm_id)
     requests.delete(ENDPOINT_DM_REMOVE, json=data)
 
     resp = requests.get(ENDPOINT_DM_LIST, {'token': get_usr_1['token']}).json()['dms']
@@ -86,7 +87,7 @@ def test_valid(get_usr_1, get_usr_2):
     assert resp == expected_output
     assert resp1 == expected_output
 
-    data = generate_dm_remove_json(get_usr_2['token'], dm_id1)
+    data = generate_dm_json(get_usr_2['token'], dm_id1)
     requests.delete(ENDPOINT_DM_REMOVE, json=data)
 
     resp = requests.get(ENDPOINT_DM_LIST, {'token': get_usr_1['token']}).json()['dms']
