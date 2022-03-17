35439bacc1e746ef286a00181624b1f4a06c604e
Hanqi Bai <z5286125@ad.unsw.edu.au>
2022-03-17 16:38:53 +1100

    add double hash

--- src/encrypt.py
+++ src/encrypt.py
@@ -2,8 +2,8 @@
     example:
     password:           -------------------------------------------------------------------
     selted_password:    k--f--y--u--s--r--e--1--d--f--6--g--6--8--s--e--r--d--s--f--3--2--g--1--8--5--a--w--e--9--6--1--a--3-
-    encode_password:    b'k--f--y--u--s--r--e--1--d--f--6--g--6--8--s--e--r--d--s--f--3--2--g--1--8--5--a--w--e--9--6--1--a--3-'
     hashed_password:    a2bef8ef61b1818af4e4c28fc008d69ab6f77f443bf58a04948cac54f2143525
+    double_hashed:      0d4833fbd1c8e7985d34c69b233eff7a9703586698fcef1f82d215acda7e49a8
 """
 
 import hashlib
@@ -19,7 +19,7 @@ Input:
 Process:
     Add random string if password length is less than 10.
     Insert random character to the password for every two characters in the password
-    Encrypt with sha256
+    Encrypt with double sha256
 
 Return value:
     Encrypted password (str)
@@ -38,5 +38,6 @@ def hashing_password(password):
                 idx += 1
         selted_password += password[i]
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
-    return hashed_password
+    dubl_hashed_password = hashlib.sha256(hashed_password.encode()).hexdigest()
+    return dubl_hashed_password
 
