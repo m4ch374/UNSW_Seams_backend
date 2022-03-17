661305987ce0019550b25c58606d58c5524bfb88
Henry Wan <z5338828@ad.unsw.edu.au>
2022-03-17 19:21:54 +0800

    Fix: added pytest decorator to function

--- tests/iteration2_tests/conftest.py
+++ tests/iteration2_tests/conftest.py
@@ -56,6 +56,7 @@ def get_usr_2():
     resp = requests.post(ENDPOINT_REGISTER_USR, json=REGISTER_DETAILS_2).json()
     return resp
 
+@pytest.fixture
 def get_u_id():
     resp = requests.post(ENDPOINT_REGISTER_USR, json=REGISTER_DETAILS_3).json()
     return {"id": resp['auth_user_id'], "token": resp['token']}
