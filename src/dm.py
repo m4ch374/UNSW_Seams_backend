a21d8d7f048763c18f5b9d842b4cc5f6dd457bd5
Henry Wan <z5338828@ad.unsw.edu.au>
2022-03-17 22:16:16 +0800

    Style: repeated code are substitued with functions

--- src/dm.py
+++ src/dm.py
@@ -8,6 +8,20 @@ from src.data_store import data_store
 from src.error import InputError, AccessError
 from src.objecs import DmChannel
 
+# =================== helpers ======================
+def check_valid_dm_id(dm_id):
+    if not data_store.has_dm_id(dm_id):
+        raise InputError(description="error: Invalid dm id.")
+
+def check_is_creator(dm_chnl, u_id):
+    if not (dm_chnl.has_owner_id(u_id) and dm_chnl.has_member_id(u_id)):
+        raise AccessError(description="error: ID is not the creator or no longer in DM.")
+
+def check_is_member(dm_chnl, u_id):
+    if not dm_chnl.has_member_id(u_id):
+        raise AccessError(description="error: ID not a member of dm.")
+# ==================================================
+
 def dm_create_v1(u_id, u_ids):
     # Create dm channel
     dm_chnl = DmChannel(data_store.get_user(u_id), u_ids)
@@ -21,33 +35,30 @@ def dm_list_v1(u_id):
     return {'dms': dms}
 
 def dm_remove_v1(u_id, dm_id):
-    if not data_store.has_dm_id(dm_id):
-        raise InputError(description="error: Invalid dm id.")
+    check_valid_dm_id(dm_id)
 
     dm_chnl = data_store.get_dm(dm_id)
-    if not (dm_chnl.has_owner_id(u_id) and dm_chnl.has_member_id(u_id)):
-        raise AccessError(description="error: ID is not the creator or no longer in DM.")
+    
+    check_is_creator(dm_chnl, u_id)
 
     data_store.remove_dm(dm_chnl)
     return {}
 
 def dm_details_v1(u_id, dm_id):
-    if not data_store.has_dm_id(dm_id):
-        raise InputError(description="error: Invalid dm id")
+    check_valid_dm_id(dm_id)
 
     dm_chnl = data_store.get_dm(dm_id)
-    if not dm_chnl.has_member_id(u_id):
-        raise AccessError(description="error: ID not a member of dm.")
+    
+    check_is_member(dm_chnl, u_id)
 
     return dm_chnl.channel_details_dict()
 
 def dm_leave_v1(u_id, dm_id):
-    if not data_store.has_dm_id(dm_id):
-        raise InputError(description="error: Invalid dm id")
+    check_valid_dm_id(dm_id)
     
     dm_chnl = data_store.get_dm(dm_id)
-    if not dm_chnl.has_member_id(u_id):
-        raise AccessError(description="error: ID not a member of dm.")
+    
+    check_is_member(dm_chnl, u_id)
 
     dm_chnl.remove_member_id(u_id)
     return {}
