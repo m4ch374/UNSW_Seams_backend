39d9f5e2358746b0310657ffcb5f7293a0cfaa51
James <z5360589@ad.unsw.edu.au>
2022-03-22 17:49:12 +1100

    written tests for admin user perm change

---
+++ tests/iteration2_tests/admin_tests/definitions.py
@@ -0,0 +1,12 @@
+'''
+Definitions used for admin functions
+'''
+# Two global permissions types
+OWNER_PERM_ID = 1
+USER_PERM_ID = 2
+# Invalid token (used for channel functions testing)
+INVALID_TOKEN = "bleh"
+# Invalid user id (used when no valid users have already been created)
+INVALID_U_ID = -10000
+# Invalid permission_id
+INVALID_PERM_ID = -10000 
