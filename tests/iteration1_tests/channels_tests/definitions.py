3340714d955661211ffe48f45098a21ec1a5e620
Hanqi Bai <z5286125@ad.unsw.edu.au>
2022-03-22 20:59:26 +1100

    comment iteration1 tests for coverage

--- tests/iteration1_tests/channels_tests/definitions.py
+++ tests/iteration1_tests/channels_tests/definitions.py
@@ -1,30 +1,30 @@
-"""
-This file contains definitions used across scope `channels`
-"""
+# """
+# This file contains definitions used across scope `channels`
+# """
 
-# A list of channel names that could cause errors
-ERROR_LIST = [
-        None,                               # NULL
-        "",                                 # Empty name
-        "abcdefghijklmnopqrstu",            # 21 chars
-        "...............................",  # non english chars
-        "                               ",  # whitespaces with len > 20
-        "123456789012345678901",            # numbers
-        "!!!!!!!ldjfljasdlkjklsdj894",      # special chars, english chars and nums
-    ]
+# # A list of channel names that could cause errors
+# ERROR_LIST = [
+#         None,                               # NULL
+#         "",                                 # Empty name
+#         "abcdefghijklmnopqrstu",            # 21 chars
+#         "...............................",  # non english chars
+#         "                               ",  # whitespaces with len > 20
+#         "123456789012345678901",            # numbers
+#         "!!!!!!!ldjfljasdlkjklsdj894",      # special chars, english chars and nums
+#     ]
 
-# A list of channel names that are valid
-NAMES_LIST = [
-        "a",                    # single character
-        "abcdef",               # english character
-        "123456",               # nums
-        "there is something",   # str with whitespaces
-        "           ",          # str with all whitespaces
-        "1234567890abcdefg![]", # combined strings, len == 20
-        "trailing whitespace ", # str with trailing whitespace
-        "a",                    # duplications
-        "!!!!!!![[[]]]!!!!!!!", # all special chars
-    ]
+# # A list of channel names that are valid
+# NAMES_LIST = [
+#         "a",                    # single character
+#         "abcdef",               # english character
+#         "123456",               # nums
+#         "there is something",   # str with whitespaces
+#         "           ",          # str with all whitespaces
+#         "1234567890abcdefg![]", # combined strings, len == 20
+#         "trailing whitespace ", # str with trailing whitespace
+#         "a",                    # duplications
+#         "!!!!!!![[[]]]!!!!!!!", # all special chars
+#     ]
 
-# An invalid id
-INVALID_ID = -1
+# # An invalid id
+# INVALID_ID = -1
