d0ad7b681f26318ad27dc83a15537fbbc5df8c0c
James <z5360589@ad.unsw.edu.au>
2022-03-26 15:10:10 +1100

    written tests for admin_user_remove

--- tests/iteration2_tests/admin_tests/admin_user_remove_v1_test.py
+++ tests/iteration2_tests/admin_tests/admin_user_remove_v1_test.py
@@ -18,14 +18,15 @@ from src.error import InputError, AccessError
 from tests.iteration2_tests.endpoints import (
     ENDPOINT_ADMIN_REMOVE, ENDPOINT_JOIN_CHNL, ENDPOINT_LIST_CHNL,
     ENDPOINT_CREATE_CHNL, ENDPOINT_DM_CREATE, ENDPOINT_USER_PROF,
-    ENDPOINT_DM_DETAILS, ENDPOINT_REGISTER_USR, ENDPOINT_USERS_ALL
+    ENDPOINT_DM_DETAILS, ENDPOINT_USERS_ALL, ENDPOINT_MESSAGE_SEND,
 )
 from tests.iteration2_tests.admin_tests.definitions import (
     INVALID_TOKEN, INVALID_U_ID,
 )
 from tests.iteration2_tests.helper import (
     create_admin_remove_user_input_json, create_chnl_join_input_json,
-    generate_channel_input_json, generate_dm_input_json, generate_dm_json
+    generate_channel_input_json, generate_dm_input_json, generate_dm_json,
+    generate_get_channel_message_url, send_msg_json,
 )
 
 # raise AccessError since invalid token passed
@@ -66,7 +67,7 @@ def test_admin_remove_user_v1_simple_code_test(get_token_1, get_u_id):
     assert response.status_code == 200
 
 # remove a user, and assert that they are removed from all channels
-def test_admin_remove_user_v1_from_channels(get_token_1, get_u_id):
+def test_admin_remove_user_v1_from_channels(get_token_1, get_u_id, get_u_id2):
     # get_token_1 user create channel
     data1 = generate_channel_input_json(get_token_1, "First Chnl", True)
     response = requests.post(ENDPOINT_CREATE_CHNL, json=data1).json()
@@ -74,6 +75,12 @@ def test_admin_remove_user_v1_from_channels(get_token_1, get_u_id):
     # join get_u_id user to the first channel created by fixture
     json_input = create_chnl_join_input_json(get_u_id['token'], channel_id)
     requests.post(ENDPOINT_JOIN_CHNL, json = json_input)
+    # create a new chnl with get_u_id as owner
+    data2 = generate_channel_input_json(get_u_id['token'], "second Chnl", True)
+    requests.post(ENDPOINT_CREATE_CHNL, json=data2)
+    # create a new chnl with get_u_id2 as owner (for coverage sake)
+    data3 = generate_channel_input_json(get_u_id2['token'], "third Chnl", True)
+    requests.post(ENDPOINT_CREATE_CHNL, json=data3)
     # remove the user that was just inserted
     json_input = create_admin_remove_user_input_json(get_token_1,
                                                      get_u_id['id'])
@@ -83,32 +90,40 @@ def test_admin_remove_user_v1_from_channels(get_token_1, get_u_id):
     assert channel_list == []
 
 # test that a removed user is removed from all dms
-def test_admin_remove_user_v1_not_in_dms(get_usr_1, get_usr_2):
+def test_admin_remove_user_v1_not_in_dms(get_usr_1, get_usr_2, get_u_id):
     # create a dm with two users
     data = generate_dm_input_json(get_usr_1['token'], [get_usr_2['auth_user_id']])
     dm_id = requests.post(ENDPOINT_DM_CREATE, json=data).json()['dm_id']
-    # remove the user get_usr_2 from existence
-    json_input = create_admin_remove_user_input_json(get_usr_1,
-                                                     get_usr_2['auth_user_id'])
-    response = requests.delete(ENDPOINT_ADMIN_REMOVE, json = json_input)
-    # test that the removed user is no longer in the original dm
+    # create another dm without get_usr_2 for coverages sake
+    data2 = generate_dm_input_json(get_usr_1['token'], [get_u_id['id']])
+    requests.post(ENDPOINT_DM_CREATE, json=data2)
+    # create the expected data for once user is removed
     data = generate_dm_json(get_usr_1['token'], dm_id)
-    resp = requests.get(ENDPOINT_DM_DETAILS, data).json()
-    usr1 = requests.get(ENDPOINT_USER_PROF, {'token': get_usr_1['token'], 'u_id': get_usr_1['auth_user_id']}).json()['user']
-    usr2 = requests.get(ENDPOINT_USER_PROF, {'token': get_usr_2['token'], 'u_id': get_usr_2['auth_user_id']}).json()['user']
-    
+    prof1_dict = {'token': get_usr_1['token'],
+                  'u_id': get_usr_1['auth_user_id']}
+    prof2_dict = {'token': get_usr_2['token'],
+                  'u_id': get_usr_2['auth_user_id']}
+    usr1 = requests.get(ENDPOINT_USER_PROF, prof1_dict).json()['user']
+    usr2 = requests.get(ENDPOINT_USER_PROF, prof2_dict).json()['user']
+
     expected_name = ', '.join(sorted([usr1['handle_str'], usr2['handle_str']]))
     expected_output = {
         'name': expected_name,
         'members': [usr1],
     }
+    # remove the user get_usr_2 from existence
+    json_input = create_admin_remove_user_input_json(get_usr_1['token'],
+                                                     get_usr_2['auth_user_id'])
+    requests.delete(ENDPOINT_ADMIN_REMOVE, json = json_input)
+    # fetch the dm details
+    resp = requests.get(ENDPOINT_DM_DETAILS, data).json()
     assert resp == expected_output
 
 
 # test that removed user will not be included in the list of users returned by
 # users/all
 def test_admin_remove_user_v1_via_user_all(get_usr_1, get_usr_2):
-    json_input = create_admin_remove_user_input_json(get_usr_1,
+    json_input = create_admin_remove_user_input_json(get_usr_1['token'],
                                                      get_usr_2['auth_user_id'])
     requests.delete(ENDPOINT_ADMIN_REMOVE, json = json_input)
     response = requests.get(ENDPOINT_USERS_ALL, {'token': get_usr_1['token']})
@@ -118,30 +133,44 @@ def test_admin_remove_user_v1_via_user_all(get_usr_1, get_usr_2):
                                   'name_first': 'joe',
                                   'name_last': 'bidome',
                                   'handle_str': 'joebidome'}]}
-    print(response_data)
-    print(expected_output)
     assert response_data == expected_output
 
-# test that the profile of a removed user must still be retrievable with 
+# test that the profile of a removed user must still be retrievable with
 # user/profile, however name_first should be 'Removed' and 'name_last' should
 # be 'user'.
 def test_admin_remove_user_v1_new_profile(get_usr_1, get_usr_2):
     # remove user via admin remove function
     json_input = create_admin_remove_user_input_json(get_usr_1['token'],
                                                      get_usr_2['auth_user_id'])
+    requests.delete(ENDPOINT_ADMIN_REMOVE, json = json_input)
+    # verify that user profile exists and is updated
     user_2_id = get_usr_2['auth_user_id']
     user_2_tok = get_usr_2['token']
-    requests.delete(ENDPOINT_ADMIN_REMOVE, json = json_input)
-    # verify that user profile exists and is updated 
     response = requests.get(ENDPOINT_USER_PROF, {'token': user_2_tok,
                                                  'u_id': user_2_id})
     assert response.status_code == 200
     response_data = response.json()
-    
     assert (response_data['user'])['name_first'] == 'Removed'
     assert (response_data['user'])['name_last'] == 'user'
-# Once users are removed, the contents of the messages they sent will be replaced by 'Removed user'.
-'''
-NOT YET AVAILABLE SINCE DM MESSAGES DOES NOT YET EXIST :(
-'''
 
+# test that once users are removed, the contents of the messages they sent
+# will be replaced by 'Removed user'.
+def test_admin_remove_user_v1_modified_messages(get_u_id, get_usr_1):
+    data1 = generate_channel_input_json(get_usr_1['token'], "First Chnl", True)
+    response = requests.post(ENDPOINT_CREATE_CHNL, json=data1).json()
+    channel_id = response['channel_id']
+    token = get_usr_1['token']
+    for _ in range(2):
+        requests.post(ENDPOINT_MESSAGE_SEND, json=send_msg_json(token, channel_id, 'a'))
+    # join get_u_id user to the first channel created by fixture
+    json_input = create_chnl_join_input_json(get_u_id['token'], channel_id)
+    requests.post(ENDPOINT_JOIN_CHNL, json = json_input)
+    # remove the original message creator
+    json_input = create_admin_remove_user_input_json(get_u_id['token'],
+                                                     get_usr_1['auth_user_id'])
+    requests.delete(ENDPOINT_ADMIN_REMOVE, json = json_input)
+    # verify messages of removed user have been modified
+    response = requests.get(generate_get_channel_message_url(get_u_id['token'], channel_id, 0))
+    dict_of_messages = (response.json())['messages']
+    for i in range(2):
+        assert dict_of_messages[i]['message'] == 'Removed user'
