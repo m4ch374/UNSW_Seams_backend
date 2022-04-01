ddb07d2fa6a4bdb4e5efc0598a255989edaf13ab
Hanqi Bai <z5286125@ad.unsw.edu.au>
2022-04-02 04:55:51 +1100

    finish tests

--- tests/iteration3_tests/user_tests/user_stats_test.py
+++ tests/iteration3_tests/user_tests/user_stats_test.py
@@ -1,10 +1,9 @@
 import requests
-from tests.iteration3_tests.user_tests.definitions import REGISTER_V2, USER_STATS_V1
+from tests.iteration3_tests.user_tests.definitions import REGISTER_V2, USER_STATS_V1, ENDPOINT_CREATE_CHNL, ENDPOINT_CHNL_INVITE, ENDPOINT_MESSAGE_SEND
 
 
 def test_invalid_token():
-    json = {'token': 'xxxxxxxxxxxx'}
-    response = requests.post(USER_STATS_V1, json = json)
+    response = requests.get(USER_STATS_V1, {'token': 'xxxxxxxxxxxx'})
     assert response.status_code == 403
 
 def test_valid_input():
@@ -15,12 +14,14 @@ def test_valid_input():
     user = requests.post(REGISTER_V2, json = {'email': 'z5555555@ed.unsw.edu.au', 'password': '1234567', 'name_first': 'William', 'name_last': 'Wu'})
     user_data = user.json()
     #TODO
-    json = {'token': user_data['token']}
-    response = requests.post(USER_STATS_V1, json = json)
+    requests.post(ENDPOINT_CREATE_CHNL, json = {'token':user_data['token'],'name':"123", 'is_public':False})
+    requests.post(ENDPOINT_CHNL_INVITE, json = {'token':user_data['token'],'channel_id':1, 'u_id':2})
+    requests.post(ENDPOINT_MESSAGE_SEND, json = {'token':user_data['token'],'channel_id':1, 'message':'qqq'})
+    response = requests.get(USER_STATS_V1, {'token': user_data['token']})
     assert response.status_code == 200
-    user_stats = response.json()['user_stats']
-    assert user_stats['channels_joined'] == []
-    assert user_stats['dms_joined'] == []
-    assert user_stats['messages_sent'] == []
-    assert type(user_stats['involvement_rate']) == type(1.2345)
+    workspace_stats = response.json()['user_stats']
+    assert workspace_stats['channels_joined'][0]['num_channels_joined'] == 1
+    assert workspace_stats['dms_joined'][0]['num_dms_joined'] == 0
+    assert workspace_stats['messages_sent'][0]['num_messages_sent'] == 1
+    assert type(workspace_stats['involvement_rate']) == type(1.2345)
 
