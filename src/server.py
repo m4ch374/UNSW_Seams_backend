b33e03b3b7021bce367585f06dc47b21d66f441b
Hanqi Bai <z5286125@ad.unsw.edu.au>
2022-04-02 05:39:42 +1100

    write up some of the new funcs

--- src/server.py
+++ src/server.py
@@ -357,4 +357,4 @@ def clear():
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, quit_gracefully) # For coverage
-    APP.run(port=config.port, debug=True, threaded=True) # Do not edit this port
+    APP.run(port=config.port, debug=False, threaded=True) # Do not edit this port
