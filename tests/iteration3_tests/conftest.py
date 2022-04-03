433a871cc322a84409ff2392b48135fac76c7f79
Henry Wan <z5338828@ad.unsw.edu.au>
2022-04-04 00:41:13 +1000

    Fixtures: added fixtures

--- tests/iteration3_tests/conftest.py
+++ tests/iteration3_tests/conftest.py
@@ -7,7 +7,22 @@ import pytest
 import requests
 
 # Import definitions
-from tests.iteration3_tests.endpoints import ENDPOINT_CLEAR
+from tests.iteration3_tests.endpoints import *
+
+# =============== Local Definitions ================
+REGISTER_DETAILS_1 = { 
+    'email': 'randomemail@gmail.com',
+    'password': 'thisisapassword', 
+    'name_first': 'joe',
+    'name_last': 'bidome',
+}
+
+REGISTER_DETAILS_2 = {
+    'email': 'anotherrandomemail@gmail.com',
+    'password': 'verysecurepassword',
+    'name_first': 'Obama',
+    'name_last': 'Prism',
+}
 
 # ==================================================
 
@@ -15,3 +30,12 @@ from tests.iteration3_tests.endpoints import ENDPOINT_CLEAR
 def clear():
     requests.delete(ENDPOINT_CLEAR)
 
+@pytest.fixture
+def get_usr_1():
+    resp = requests.post(ENDPOINT_REGISTER_USR, json=REGISTER_DETAILS_1).json()
+    return resp
+
+@pytest.fixture
+def get_usr_2():
+    resp = requests.post(ENDPOINT_REGISTER_USR, json=REGISTER_DETAILS_2).json()
+    return resp
