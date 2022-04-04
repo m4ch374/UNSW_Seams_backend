41dd772146c64c7dbd66a88dab646399a5b3ba64
James <z5360589@ad.unsw.edu.au>
2022-04-04 14:21:48 +1000

    wrote test test for mesage stub

--- tests/iteration3_tests/message_tests/message_pin_v1_test.py
+++ tests/iteration3_tests/message_tests/message_pin_v1_test.py
@@ -14,11 +14,33 @@
 # =============================================================================
 '''
 # imports used
+import requests
+from src.error import InputError, AccessError
+from tests.iteration3_tests.endpoints import (
+    ENDPOINT_MESSAGE_PIN,
+)
+from tests.iteration3_tests.message_tests.definitions import (
+    INVALID_TOKEN, INVALID_MSG_ID,
+)
+from tests.iteration3_tests.helper import (
+    create_msg_pin_input_json,
+)
 
-
-
-
-
-
+# raise AccessError since invalid token passed
+#
+# while invalid msg should raise input error, accesserror takes precedent
+def test_message_pin_v1_invalid_token():
+    json_input = create_msg_pin_input_json(INVALID_TOKEN, INVALID_MSG_ID)
+    response = requests.post(ENDPOINT_MESSAGE_PIN, json = json_input)
+    assert response.status_code == AccessError.code
+'''
+# raise input error since message id is not a valid message within a chnl user
+# has joined
+def test_message_pin_v1_invalid_msg_id(user_1_made_channel):
+    user1_tok = user_1_made_channel['token']
+    json_input = create_msg_pin_input_json(user1_tok, INVALID_MSG_ID)
+    response = requests.post(ENDPOINT_MESSAGE_PIN, json = json_input)
+    print(response)
+'''
 
 
--- tests/iteration3_tests/message_tests/message_unpin_v1_test.py
+++ tests/iteration3_tests/message_tests/message_unpin_v1_test.py
@@ -7,7 +7,7 @@
 # InputError when:
 #   - message_id is not a valid message within a channel or DM that the 
 #     authorised user has joined
-#   - the message is already pinned
+#   - the message is not already pinned
 # AccessError when:
 #   - message_id refers to a valid message in a joined channel/DM and the 
 #     authorised user does not have owner permissions in the channel/DM
@@ -17,23 +17,21 @@
 import requests
 from src.error import InputError, AccessError
 from tests.iteration3_tests.endpoints import (
-    
+    ENDPOINT_MESSAGE_PIN, ENDPOINT_MESSAGE_UNPIN
 )
 from tests.iteration3_tests.message_tests.definitions import (
-    INVALID_TOKEN, INVALID_MSG_ID, INVALID_U_ID,
+    INVALID_TOKEN, INVALID_MSG_ID,
 )
 from tests.iteration3_tests.helper import (
-    
+    create_msg_pin_input_json,
 )
 
-
-
-
-
-
-
-
-
-
-
-
+# raise AccessError since invalid token passed
+#
+# while invalid msg should raise input error, accesserror takes precedent
+'''
+def test_message_unpin_v1_invalid_token():
+    json_input = create_msg_pin_input_json(INVALID_TOKEN, INVALID_MSG_ID)
+    reponse = requests.post(ENDPOINT_MESSAGE_PIN, json = json_input)
+    assert response.status_code == AccessError.code
+'''
