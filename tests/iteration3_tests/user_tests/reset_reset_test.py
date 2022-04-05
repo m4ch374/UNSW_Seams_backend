3f22afc40dc03c57760ef87fe8178d188cfe9c14
Hanqi Bai <z5286125@ad.unsw.edu.au>
2022-04-06 05:47:50 +1000

    -

--- tests/iteration3_tests/user_tests/reset_reset_test.py
+++ tests/iteration3_tests/user_tests/reset_reset_test.py
@@ -1,5 +1,5 @@
 import requests, time, imaplib, email
-from tests.iteration3_tests.user_tests.definitions import REGISTER_V2, AUTH_PASSWORDRESET_RESET_V1, AUTH_PASSWORDRESET_REQUEST_V1
+from tests.iteration3_tests.endpoints import ENDPOINT_REGISTER_USR, ENDOPINT_PWORD_RESET, ENDPOINT_PWORD_REQUEST
 from src.config import SERVER_EMAIL, SERVER_PASSWORD
 
 
@@ -18,26 +18,26 @@ def get_code():
     return msgline[0][22:27]
 
 def test_invalid_reset_code():
-    requests.post(REGISTER_V2, json = {'email': 'z8888888@ed.unsw.edu.au', 'password': '12345678', 'name_first': 'Russell', 'name_last': 'Wang'})
-    requests.post(AUTH_PASSWORDRESET_REQUEST_V1, json = {'email': 'z8888888@ed.unsw.edu.au'})
-    response = requests.post(AUTH_PASSWORDRESET_RESET_V1, json = {'reset_code': '123', 'new_password': '1234567'})
+    requests.post(ENDPOINT_REGISTER_USR, json = {'email': 'z8888888@ed.unsw.edu.au', 'password': '12345678', 'name_first': 'Russell', 'name_last': 'Wang'})
+    requests.post(ENDPOINT_PWORD_REQUEST, json = {'email': 'z8888888@ed.unsw.edu.au'})
+    response = requests.post(ENDOPINT_PWORD_RESET, json = {'reset_code': '123', 'new_password': '1234567'})
     assert response.status_code == 400
 
 def test_short_password():
-    requests.post(REGISTER_V2, json = {'email': SERVER_EMAIL, 'password': '12345678', 'name_first': 'Russell', 'name_last': 'Wang'})
-    requests.post(AUTH_PASSWORDRESET_REQUEST_V1, json = {'email': SERVER_EMAIL})
+    requests.post(ENDPOINT_REGISTER_USR, json = {'email': SERVER_EMAIL, 'password': '12345678', 'name_first': 'Russell', 'name_last': 'Wang'})
+    requests.post(ENDPOINT_PWORD_REQUEST, json = {'email': SERVER_EMAIL})
     time.sleep(5)
     reset_code = get_code()
-    response = requests.post(AUTH_PASSWORDRESET_RESET_V1, json = {'reset_code': reset_code, 'new_password': '123'})
+    response = requests.post(ENDOPINT_PWORD_RESET, json = {'reset_code': reset_code, 'new_password': '123'})
     assert response.status_code == 400
 
 def test_valid_input():
-    requests.post(REGISTER_V2, json = {'email': 'z8888888@ed.unsw.edu.au', 'password': '12345678', 'name_first': 'Russell', 'name_last': 'Wang'})
-    requests.post(REGISTER_V2, json = {'email': SERVER_EMAIL, 'password': '12345678', 'name_first': 'Russell', 'name_last': 'Wang'})
-    requests.post(AUTH_PASSWORDRESET_REQUEST_V1, json = {'email': SERVER_EMAIL})
+    requests.post(ENDPOINT_REGISTER_USR, json = {'email': 'z8888888@ed.unsw.edu.au', 'password': '12345678', 'name_first': 'Russell', 'name_last': 'Wang'})
+    requests.post(ENDPOINT_REGISTER_USR, json = {'email': SERVER_EMAIL, 'password': '12345678', 'name_first': 'Russell', 'name_last': 'Wang'})
+    requests.post(ENDPOINT_PWORD_REQUEST, json = {'email': SERVER_EMAIL})
     time.sleep(5)
     reset_code = get_code()
-    response = requests.post(AUTH_PASSWORDRESET_RESET_V1, json = {'reset_code': reset_code, 'new_password': '123456'})
+    response = requests.post(ENDOPINT_PWORD_RESET, json = {'reset_code': reset_code, 'new_password': '123456'})
     assert response.status_code == 200
     assert response.json() == {}
 
--- tests/iteration3_tests/user_tests/search_test.py
+++ tests/iteration3_tests/user_tests/search_test.py
@@ -1,44 +1,44 @@
 import requests
-from tests.iteration3_tests.user_tests.definitions import REGISTER_V2, SEARCH, ENDPOINT_CHNL_INVITE, ENDPOINT_CREATE_CHNL, ENDPOINT_MESSAGE_SEND, ENDPOINT_DM_CREATE, ENDPOINT_DM_SEND
+from tests.iteration3_tests.endpoints import ENDPOINT_REGISTER_USR, ENDOPINT_SEARCH, ENDPOINT_CHNL_INVITE, ENDPOINT_CREATE_CHNL, ENDPOINT_MESSAGE_SEND, ENDPOINT_DM_CREATE, ENDPOINT_DM_SEND
 
 
 def test_invalid_token():
-    response = requests.get(SEARCH, {'token': '123', 'query_str': "123213"})
+    response = requests.get(ENDOPINT_SEARCH, {'token': '123', 'query_str': "123213"})
     assert response.status_code == 403
 
 def test_invalid_query_str():
-    user = requests.post(REGISTER_V2, json = {'email': 'z7654321@ed.unsw.edu.au', 'password': '1234567', 'name_first': 'Jason', 'name_last': 'Smith'})
+    user = requests.post(ENDPOINT_REGISTER_USR, json = {'email': 'z7654321@ed.unsw.edu.au', 'password': '1234567', 'name_first': 'Jason', 'name_last': 'Smith'})
     assert user.status_code == 200
     user_data = user.json()
-    response = requests.get(SEARCH, {'token': user_data['token'], 'query_str': ''})
+    response = requests.get(ENDOPINT_SEARCH, {'token': user_data['token'], 'query_str': ''})
     assert response.status_code == 400
-    response = requests.get(SEARCH, {'token': user_data['token'], 'query_str': 'q' * 1001})
+    response = requests.get(ENDOPINT_SEARCH, {'token': user_data['token'], 'query_str': 'q' * 1001})
     assert response.status_code == 400
 
 def test_valid_input_ch():
-    user = requests.post(REGISTER_V2, json = {'email': 'z1@ed.unsw.edu.au', 'password': '1234567', 'name_first': '11', 'name_last': '11'})
-    user2 = requests.post(REGISTER_V2, json = {'email': 'z2@ed.unsw.edu.au', 'password': '1234567', 'name_first': '22', 'name_last': '22'}).json()
+    user = requests.post(ENDPOINT_REGISTER_USR, json = {'email': 'z1@ed.unsw.edu.au', 'password': '1234567', 'name_first': '11', 'name_last': '11'})
+    user2 = requests.post(ENDPOINT_REGISTER_USR, json = {'email': 'z2@ed.unsw.edu.au', 'password': '1234567', 'name_first': '22', 'name_last': '22'}).json()
     user_data = user.json()
     requests.post(ENDPOINT_CREATE_CHNL, json = {'token':user_data['token'],'name':"123", 'is_public':False})
     requests.post(ENDPOINT_CREATE_CHNL, json = {'token':user2['token'],'name':"1234", 'is_public':False})
     requests.post(ENDPOINT_CHNL_INVITE, json = {'token':user_data['token'],'channel_id':1, 'u_id':2})
     requests.post(ENDPOINT_MESSAGE_SEND, json = {'token':user_data['token'],'channel_id':1, 'message':'qqq'})
     requests.post(ENDPOINT_MESSAGE_SEND, json = {'token':user_data['token'],'channel_id':1, 'message':'zzzz'})
-    response = requests.get(SEARCH, {'token': user_data['token'], 'query_str': 'q'})
-    requests.get(SEARCH, {'token': user2['token'], 'query_str': 'q'})
+    response = requests.get(ENDOPINT_SEARCH, {'token': user_data['token'], 'query_str': 'q'})
+    requests.get(ENDOPINT_SEARCH, {'token': user2['token'], 'query_str': 'q'})
     assert response.status_code == 200
     response_data = response.json()
     assert len(response_data['messages']) == 1
 
 def test_valid_input_dm():
-    user = requests.post(REGISTER_V2, json = {'email': 'z1@ed.unsw.edu.au', 'password': '1234567', 'name_first': '11', 'name_last': '11'})
-    user2 = requests.post(REGISTER_V2, json = {'email': 'z2@ed.unsw.edu.au', 'password': '1234567', 'name_first': '22', 'name_last': '22'}).json()
+    user = requests.post(ENDPOINT_REGISTER_USR, json = {'email': 'z1@ed.unsw.edu.au', 'password': '1234567', 'name_first': '11', 'name_last': '11'})
+    user2 = requests.post(ENDPOINT_REGISTER_USR, json = {'email': 'z2@ed.unsw.edu.au', 'password': '1234567', 'name_first': '22', 'name_last': '22'}).json()
     user_data = user.json()
     requests.post(ENDPOINT_DM_CREATE, json = {'token':user_data['token'],'u_ids':[]})
     requests.post(ENDPOINT_DM_CREATE, json = {'token':user2['token'],'u_ids':[]})
     requests.post(ENDPOINT_DM_SEND, json = {'token':user_data['token'],'dm_id':1, 'message':'qqq'})
     requests.post(ENDPOINT_DM_SEND, json = {'token':user2['token'],'dm_id':2, 'message':'qqqq'})
-    response = requests.get(SEARCH, {'token': user_data['token'], 'query_str': 'q'})
+    response = requests.get(ENDOPINT_SEARCH, {'token': user_data['token'], 'query_str': 'q'})
     assert response.status_code == 200
     response_data = response.json()
     assert len(response_data['messages']) == 1
