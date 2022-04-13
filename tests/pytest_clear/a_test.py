bf58311d305efaba15f55fd610b47d6e44bc0d09
Hanqi Bai <z5286125@ad.unsw.edu.au>
2022-04-13 17:10:59 +1000

    roll back

--- tests/pytest_clear/a_test.py
+++ tests/pytest_clear/a_test.py
@@ -10,37 +10,3 @@ ENDPOINT_CLEAR = url + '/clear/v1'
 def test_clear():
     requests.delete(ENDPOINT_CLEAR)
     pass
-
-
-
-import json, os
-from src.objecs import User, Channel, DmChannel, Message
-
-"""
-    Data persistence and functional functionality have been tested,
-    this just for coverage,
-    considering the server will not restart during pytest.
-"""
-
-
-def test_json():
-    CURRENT_DIR = os.path.dirname(os.path.realpath(__file__)) + '/'
-    DATA_PATH = CURRENT_DIR + 'a.json'
-    file_content = open(DATA_PATH, "r")
-    JSON = json.load(file_content)
-
-    JSON['users'] = [User.decode_json(item) for item in JSON['users']]
-
-    id_to_usr = lambda x : next((usr for usr in JSON['users'] if usr.id == x), None)
-    get_users = lambda li: [id_to_usr(u_id) for u_id in li]
-
-    JSON['channel'] = [Channel.decode_json(item, get_users(item['owners']), get_users(item['members']))
-        for item in JSON['channel']]
-
-    JSON['dm'] = [DmChannel.decode_json(item, get_users(item['owners']), get_users(item['members']))
-        for item in JSON['dm']]
-
-    JSON['messages'] = [Message.decode_json(item) for item in JSON['messages']]
-    # Notification.decode_json(JSON['Notification'])
-    pass
-
