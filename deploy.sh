c3cb2c7cf2f86edb1e118e14ef645aa8915ba4a9
Henry Wan <z5338828@ad.unsw.edu.au>
2022-04-15 17:19:20 +1000

    Deployment: modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ USERNAME="magicant"
 SSH_HOST="ssh-magicant.alwaysdata.net"
 
 rm -rf ./**/__pycache__ ./**/.pytest_cache > /dev/null
-scp -r ./requirements.txt ./src "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
-ssh "$USERNAME@$SSH_HOST" "cd $WORKING_DIRECTORY && source env/bin/activate && pip3 install -r requirements.txt"
+scp -r ./src "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
+# ssh "$USERNAME@$SSH_HOST" "cd $WORKING_DIRECTORY && source env/bin/activate && pip3 install -r requirements.txt"
